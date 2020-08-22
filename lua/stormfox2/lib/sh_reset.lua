5826564a3e5bca421a08752148b4150d24e60d92
Nak <nak@live.dk>
2020-08-22 22:59:06 +0200

    Terrain Texture

---
+++ lua/stormfox2/lib/sh_reset.lua
@@ -0,0 +1,12 @@
+
+-- Allows to reset
+if _STORMFOX_POSTENTITY then
+	timer.Simple(0, function()
+		hook.Run("stormfox.InitPostEntity")
+	end)
+end
+
+hook.Add("InitPostEntity", "SF_PostEntity", function()
+	hook.Run("stormfox.InitPostEntity")
+	_STORMFOX_POSTENTITY = true
+end)
--- lua/stormfox2/lib/sh_terrain.lua
+++ lua/stormfox2/lib/sh_terrain.lua
@@ -1,19 +1,25 @@
 
--- Terrain is an object that changes the map e.g; Snow
--- 
 local meta = {}
 local terrains = {}
 StormFox.Terrain = {}
 -- Get and create
 function StormFox.Terrain.Create( sName )
 	local t = {}
+	t.Name = sName
 	setmetatable(t, meta)
 	terrains[sName] = t
 	t.swap = {}
 	return t
 end
+-- Default
+local CURRENT_TERRAIN = "Clear"
+-- Terrain is an object that changes the map e.g; Snow
+if SERVER then
+	util.AddNetworkString("stormfox.terrain")
+end
 
 function StormFox.Terrain.Get( sName )
+	if not sName then return end
 	return terrains[sName]
 end
 
@@ -53,10 +59,147 @@ function meta:RenderWindowRefract64x64( fFunc )
 	self.windRenderRef64 = fFun
 end
 
+-- Texture handler
+_STORMFOX_TEXCHANGES = _STORMFOX_TEXCHANGES or {} -- List of changed materials.
+_STORMFOX_TEXORIGINAL = _STORMFOX_TEXORIGINAL or {} -- This is global, just in case.
 
--- Applies textures
-_STORMFOX_TEXCHANGES = _STORMFOX_TEXCHANGES or {} -- This is global, just in case
+local function StringTex(iTex)
+	if not iTex then return end
+	if type(iTex) == "string" then return iTex end
+	return iTex:GetName()
+end
 
+local function HasChanged( self, materialTexture )
+	local mat = self:GetName() or "unknown"
+	local b = materialTexture == "basetexture2" and 2 or 1
+	return _STORMFOX_TEXCHANGES[mat] and _STORMFOX_TEXCHANGES[mat][b] or false
+end
+
+-- We're going to overwrite SetTexture. As some mods might change the default texture.
+local mat_meta = FindMetaTable("IMaterial")
+STORMFOX_TEX_APPLY = STORMFOX_TEX_APPLY or mat_meta.SetTexture
+function mat_meta:SetTexture(materialTexture, texture)
+	-- Check if it is basetexutre or basetexture2 we're changing.
+	if materialTexture ~= "basetexture" and materialTexture ~= "basetexture2" then
+		return STORMFOX_TEX_APPLY( self, materialTexture, texture )
+	end
+	-- Overwrite the original texture list.
+	local mat = self:GetName() or "unknown"
+	if not _STORMFOX_TEXORIGINAL[mat] then _STORMFOX_TEXORIGINAL[mat] = {} end
+	if materialTexture == "basetexture" then
+		_STORMFOX_TEXORIGINAL[mat][1] = StringTex(texture)
+	else
+		_STORMFOX_TEXORIGINAL[mat][2] = StringTex(texture)
+	end
+	-- If we havn't changed the texture, allow change.
+	if not HasChanged(self, materialTexture) then
+		return STORMFOX_TEX_APPLY( self, materialTexture, texture )
+	end
+end
+
+-- Resets the material. Returns false if unable to reset.
+local function ResetMaterial( self )
+	local mat = self:GetName() or "unknown"
+	if not _STORMFOX_TEXCHANGES[mat] or not _STORMFOX_TEXORIGINAL[mat] then return false end
+	if _STORMFOX_TEXCHANGES[mat][1] and _STORMFOX_TEXORIGINAL[mat][1] then
+		STORMFOX_TEX_APPLY( self, "basetexture", _STORMFOX_TEXORIGINAL[mat][1] )
+	end
+	if _STORMFOX_TEXCHANGES[mat][2] and _STORMFOX_TEXORIGINAL[mat][2] then
+		STORMFOX_TEX_APPLY( self, "basetexture2", _STORMFOX_TEXORIGINAL[mat][2] )
+	end
+	_STORMFOX_TEXCHANGES[mat] = nil
+	return true
+end
+
+-- Set the material
+local function SetMat(self, tex1, tex2)
+	if not tex1 and not tex2 then return end
+	local mat = self:GetName() or "unknown"
+	-- Save the default texture
+	if not _STORMFOX_TEXORIGINAL[mat] then _STORMFOX_TEXORIGINAL[mat] = {} end
+	if tex1 and not _STORMFOX_TEXORIGINAL[mat][1] then
+		_STORMFOX_TEXORIGINAL[mat][1] = StringTex(self:GetTexture("$basetexture"))
+	end
+	if tex2 and not _STORMFOX_TEXORIGINAL[mat][2] then
+		_STORMFOX_TEXORIGINAL[mat][2] = StringTex(self:GetTexture("$basetexture2"))
+	end
+	-- Set texture
+	if tex1 then
+		STORMFOX_TEX_APPLY( self, "basetexture", tex1 )
+	end
+	if tex2 then
+		STORMFOX_TEX_APPLY( self, "basetexture2", tex2 )
+	end
+end
+
+-- Resets and clear materials
+function StormFox.Terrain.Reset( bNoUpdate )
+	CURRENT_TERRAIN = "Clear"
+	if SERVER and not bNoUpdate then
+		net.Start("stormfox.terrain")
+			net.WriteString( CURRENT_TERRAIN )
+		net.Broadcast()
+	end
+	if next(_STORMFOX_TEXCHANGES) == nil then return end
+	for tex,_ in pairs( _STORMFOX_TEXCHANGES ) do
+		local mat = Material( tex )
+		if not ResetMaterial( mat ) then
+			StormFox.Warning( "Can't reset [" .. tostring( mat ) .. "]." )
+		end
+	end
+	_STORMFOX_TEXCHANGES = {}
+end
+
+-- Sets the terrain
+function StormFox.Terrain.Set( sName )
+	-- Apply terrain.
+	local t = StormFox.Terrain.Get( sName )
+	if not t then
+		StormFox.Terrain.Reset()
+		return
+	end
+	StormFox.Terrain.Reset( true )
+	t:Apply()
+end
+
+-- Applies terrain
 function meta:Apply()
+	CURRENT_TERRAIN = self.Name
+	if SERVER then
+		net.Start("stormfox.terrain")
+			net.WriteString( CURRENT_TERRAIN )
+		net.Broadcast()
+	end
+	-- Swap materials
+	if self.swap then
+		for mat,tab in pairs( self.swap )
+			SetMat( mat, tab[1], tab[2] )
+		end
+	end
+	-- Set ground
+	if self.ground then
+		for materialName,tab in pairs( StormFox.Map.GetTextureTree() ) do
+			local mat = Material( materialName )
+			SetMat( mat, tab[1] and self.ground, tab[2] and self.ground )
+		end
+	end
+end
 
+-- NET
+if SERVER then
+	net.Receive("stormfox.terrain", function(len, ply) -- OI, what terrain?
+		net.Start("stormfox.terrain")
+			net.WriteString( CURRENT_TERRAIN )
+		net.Broadcast()
+	end)
+else
+	net.Receive("stormfox.terrain", function(len)
+		local sName = net.ReadString()
+		StormFox.Terrain.Set( sName )
+	end)
+	-- Ask the server
+	hook.Add("stormfox.InitPostEntity", "stormfox.terrain", function()
+		net.Start("stormfox.terrain")
+		net.SendToServer()
+	end)
 end
