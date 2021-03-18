4721b3182f22a9d3eb82795e1a15ef9d419f6be1
Nak <nak@live.dk>
2021-03-18 22:40:27 +0100

    MThread display
    - Added MThread description and display
    - Added tooltip

--- lua/stormfox2/lib/cl_menu.lua
+++ lua/stormfox2/lib/cl_menu.lua
@@ -39,7 +39,7 @@ local tabs = {
 		dash.Paint = empty
 		dash:Dock(TOP)
 		dash:SetTall(100)
-		local fps, qu, sup
+		local fps, qu, sup, mth
 		-- FPS
 			local p = vgui.Create("SF_HudRing", dash)
 			p:SetText(string.upper(language.GetPhrase("#fps")) .. ": ")
@@ -71,7 +71,7 @@ local tabs = {
 				p:SetText(language.GetPhrase("#effects") .. "\n" .. math.floor(f * 100) .. "%")
 			end
 			qu = p
-		-- Support
+		-- Support GPU
 			local p = vgui.Create("SF_HudRing", dash)
 			p:SetText(niceName(language.GetPhrase("#support")))
 			p:SetSize(74, 74)
@@ -79,22 +79,75 @@ local tabs = {
 			--p:SetColor(255,0,0)
 			local t = {render.SupportsPixelShaders_1_4(),render.SupportsVertexShaders_2_0(), render.SupportsPixelShaders_2_0(), render.SupportsHDR()}
 			local v = 0
+			local s ="#problems.no_problems"
 			for k,v2 in ipairs(t) do
-				if not v2 then break end
+				if not v2 then
+					if k == 1 then
+						s = "#problem.no_ps14"
+					elseif k == 2 then
+						s = "#problem.no_vs20"
+					elseif k == 3 then
+						s = "#problem.no_ps20"
+					else
+						s = "#problem.no_hdr"
+					end
+					
+					break
+				end
 				v = v + 1
 			end
+			p:SetTooltip(s)
 			local f = v / #t
 			p:SetValue(f)
 			local c = HSLToColor(120 * f, 1, 0.5 * f)
 			--p:SetColor(c.r,c.g,c.b)
 			p:SetText(niceName(language.GetPhrase("#support")) .. "\n" .. v .. "/" .. #t)
 			sup = p
-		
+		-- MThread
+			local p = vgui.Create("SF_HudRing", dash)
+			p:SetText(niceName(language.GetPhrase("#MThread")))
+			p:SetSize(74, 74)
+			p:SetPos(188,10)
+			--p:SetColor(255,0,0)
+			local t = {["cl_threaded_bone_setup"] = 1,
+			["cl_threaded_client_leaf_system"] = 1,
+			["r_threaded_client_shadow_manager"] = 1,
+			["r_threaded_particles"] = 1,
+			["r_threaded_renderables"] = 1,
+			["r_queued_ropes"] = 1,
+			["studio_queue_mode"] = 1,
+			["gmod_mcore_test"] = 1,
+			["mat_queue_mode"] = 2}
+			local v = 0
+			local s = ""
+			for k,v2 in pairs(t) do
+				if GetConVar(k):GetInt() ~= v2 or math.random(1, 2) == 1 then
+					s = s .. k .. " " .. v2 .. "\n"
+					continue
+				end
+				v = v + 1
+			end
+			local f = v / table.Count(t)
+			p:SetValue(f)
+			local c = HSLToColor(120 * f, 1, 0.5 * f)
+			--p:SetColor(c.r,c.g,c.b)
+			p:SetText(niceName(language.GetPhrase("#MThread")) .. "\n\n" .. v .. "/" .. table.Count(t))
+			if f < 1 then
+				p:SetTooltip(string.format(language.GetPhrase("#sf_mthreadwarning"), s))
+			else
+				p:SetTooltip(language.GetPhrase("#problems.no_problems"))
+			end
+			mth = p
+
+			
+			
 		function dash:PerformLayout(w, h)
 			local a = w / 5
-			fps:SetPos(a, h - fps:GetTall())
-			qu:SetPos(a*2, h - qu:GetTall())
-			sup:SetPos(a*3, h - sup:GetTall())
+			local x = -fps:GetTall() / 2
+			fps:SetPos(x + a, h - fps:GetTall())
+			qu:SetPos(x + a*2, h - qu:GetTall())
+			sup:SetPos(x + a*3, h - sup:GetTall())
+			mth:SetPos(x + a*4, h - sup:GetTall())
 		end
 		board:AddSetting("quality_target")
 		board:AddSetting("quality_ultra")
