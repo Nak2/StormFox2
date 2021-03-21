feb3664c6ff05d2ce13c7bbeaa16de6402a1fd5f
Nak <nak@live.dk>
2021-03-21 01:53:16 +0100

    Wire Support

---
+++ lua/entities/gmod_wire_expression2/core/custom/cl_stormfox2.lua
@@ -0,0 +1,19 @@
+-- StormFox2 E2 extension
+-- Created by Nak
+
+-- Time
+E2Helper.Descriptions["sfTime()"] = "Returns the time as a number between 0 and 1439"
+E2Helper.Descriptions["isNight()"] = "Is it night?"
+E2Helper.Descriptions["isDay()"] = "Is it day?"
+E2Helper.Descriptions["sfTimeDisplay()"] = "Returns the time as a 24h string"
+E2Helper.Descriptions["sfTimeDisplay12h()"] = "Returns the time as a 12h string"
+
+-- Weather
+E2Helper.Descriptions["isRaining()"] = "Is it raining?"
+E2Helper.Descriptions["isSnowing()"] = "Is it snowing?"
+E2Helper.Descriptions["getWeather()"] = "Returns the description of the current weahter"
+E2Helper.Descriptions["getWeatherPercent()"] = "Returns the weather amount between [0-1]"
+
+-- Wind
+E2Helper.Descriptions["getWind()"] = "Returns the wind in m/s"
+E2Helper.Descriptions["getWindBeaufort()"] = "Returns the wind in Beaufort-scale"
---
+++ lua/entities/gmod_wire_expression2/core/custom/stormfox2.lua
@@ -0,0 +1,64 @@
+-- StormFox2 E2 extension
+-- By Nak
+
+E2Lib.RegisterExtension("stormfox2", false, "Lets E2 chips use StormFox functions")
+
+__e2setcost( 3 )
+
+-- Time
+	e2function number sfTime()
+		return StormFox.Time.Get()
+	end
+	e2function number isNight()
+		return StormFox.Time.IsNight() and 1 or 0
+	end
+	e2function number isDay()
+		return StormFox.Time.IsDay() and 1 or 0
+	end
+
+	__e2setcost( 15 )
+	e2function string sfTimeDisplay()
+		return StormFox.Time.TimeToString(nil)
+	end
+
+	e2function string sfTimeDisplay12h()
+		return StormFox.Time.TimeToString(nil,true)
+	end
+
+-- Weather
+	__e2setcost( 7 )
+	local function isRaining()
+		return StormFox.Weather.GetCurrent().Name == "Rain"
+	end
+	local function isCold()
+		return StormFox.Temperature.Get() <= -2
+	end
+
+	e2function number isRaining()
+		if isCold() then return 0 end
+		return isRaining() and 1 or 0
+	end
+
+	e2function number isSnowing()
+		if not isCold() then return 0 end
+		return isRaining() and 1 or 0
+	end
+
+	__e2setcost( 10 )
+	e2function string getWeather()
+		return StormFox.Weather.GetDescription()
+	end
+
+	e2function number getWeatherPercent()
+		return StormFox.Weather.GetPercent()
+	end
+-- Wind
+	__e2setcost( 3 )
+	e2function number getWind()
+		return StormFox.Wind.GetForce()
+	end
+
+	__e2setcost( 10 )
+	e2function number getWindBeaufort()
+		return StormFox.Wind.GetBeaufort()
+	end
