e16b3039d0c905f4fb0e340c39e3e9476570067b
Nak <nak@live.dk>
2021-03-21 02:06:37 +0100

    Added SF1 clocks

---
+++ lua/entities/stormfox_clock.lua
@@ -0,0 +1,98 @@
+AddCSLuaFile()
+
+ENT.Type = "anim"
+ENT.Base = "base_anim"
+ 
+ENT.PrintName		= "Clock"
+ENT.Author			= "Nak"
+ENT.Purpose			= "A working clock"
+ENT.Instructions	= "Place it somewhere"
+ENT.Category		= "StormFox2"
+
+ENT.Editable		= true
+ENT.Spawnable		= true
+ENT.AdminOnly		= false
+
+function ENT:Initialize()
+	if SERVER then
+		self:SetModel( "models/props_trainstation/trainstation_clock001.mdl" )
+		self:PhysicsInit( SOLID_VPHYSICS )      -- Make us work with physics,
+		self:SetMoveType( MOVETYPE_VPHYSICS )   -- after all, gmod is a physics
+		self:SetSolid( SOLID_VPHYSICS )         -- Toolbox
+	end
+	self.RenderMode = 1
+	self:SetRenderMode(RENDERMODE_TRANSALPHA)
+	if WireAddon and SERVER then
+		self.Outputs = Wire_CreateOutputs(self, {
+			"Clock_24 [STRING]",
+			"Clock_12 [STRING]",
+			"Clock_raw"
+		})
+		Wire_TriggerOutput(self, "Clock_raw", StormFox.Time.Get(true))
+		Wire_TriggerOutput(self, "Clock_24", StormFox.Time.TimeToString(nil))
+		Wire_TriggerOutput(self, "Clock_12", StormFox.Time.TimeToString(nil,true))
+	end
+end
+
+if SERVER then
+	if WireAddon then
+		local function SetWire(self,data,value)
+			if self.Outputs[data].Value != value then
+				Wire_TriggerOutput(self, data, value)
+			end
+		end
+		function ENT:Think()
+			if not WireAddon then return end
+			if (self._l or 0) > SysTime() then return end
+				self._l = SysTime() + 1
+			SetWire(self, "Clock_raw", StormFox.Time.Get(true))
+			SetWire(self, "Clock_24", StormFox.Time.TimeToString(nil))
+			SetWire(self, "Clock_12", StormFox.Time.TimeToString(nil,true))
+		end
+	end
+	function ENT:SpawnFunction( ply, tr, ClassName )
+		if ( !tr.Hit ) then return end
+		local SpawnPos = tr.HitPos + tr.HitNormal * 16
+		local ent = ents.Create( ClassName )
+		ent:SetPos( SpawnPos )
+		ent:SetAngles(Angle(0,ply:EyeAngles().y + 180,0))
+		ent:Spawn()
+		ent:Activate()
+		return ent
+	end
+else
+	local mat = Material("vgui/circle")
+	local mat2 = Material("vgui/dashed_line")
+	local mat3 = Material("glass/offwndwb")
+	local mat4 = Material("stormfox2/entities/clock_material")
+	local sf = Material("stormfox/SF.png")
+	function ENT:Draw()
+		render.MaterialOverrideByIndex( 0, mat3 )
+		render.MaterialOverrideByIndex( 1, mat4 )
+		self:DrawModel()
+		render.MaterialOverrideByIndex( )
+		if ( halo.RenderedEntity() == self ) then return end
+		if not StormFox then return end
+		if not StormFox.Time then return end
+
+		local a = self:GetAngles()
+		local t = StormFox.Time.Get()
+		local h = math.floor(t / 60) -- 0 - 24
+		local m = t - h * 60 -- 0 - 60
+
+		cam.Start3D2D(self:GetPos(),self:LocalToWorldAngles(Angle(0,90,90)),0.2)
+			surface.SetMaterial(mat)
+			surface.SetDrawColor(0,0,0)
+			surface.DrawTexturedRect(-10,-10,20,20)
+
+			surface.SetMaterial(mat2)
+			-- hour arm
+			local ang = h * 30 + m / 2 + 90
+			surface.DrawTexturedRectRotated(0,0,140,4,-ang)
+
+			-- min arm
+			local ang = m * 6 + 90
+			surface.DrawTexturedRectRotated(0,0,200,4,-ang)
+		cam.End3D2D()
+	end
+end
---
+++ lua/entities/stormfox_digitalclock.lua
@@ -0,0 +1,132 @@
+AddCSLuaFile()
+ENT.Type = "anim"
+ENT.Base = "base_anim"
+ 
+ENT.PrintName		= "Digital Clock"
+ENT.Author			= "Nak"
+ENT.Purpose			= "A working clock"
+ENT.Instructions	= "Place it somewhere"
+ENT.Category		= "StormFox2"
+
+ENT.Editable		= true
+ENT.Spawnable		= true
+ENT.AdminOnly		= false
+
+ENT.RenderGroup = RENDERGROUP_TRANSLUCENT
+
+if SERVER then
+	function ENT:Initialize()
+		self:SetModel( "models/hunter/blocks/cube025x025x025.mdl" )
+		self:PhysicsInit( SOLID_VPHYSICS )      -- Make us work with physics,
+		self:SetMoveType( MOVETYPE_VPHYSICS )   -- after all, gmod is a physics
+		self:SetSolid( SOLID_VPHYSICS )         -- Toolbox
+		self:SetRenderMode(RENDERMODE_TRANSALPHA)
+	end
+	function ENT:SpawnFunction( ply, tr, ClassName )
+		if ( !tr.Hit ) then return end
+		local SpawnPos = tr.HitPos + tr.HitNormal * 16
+		local ent = ents.Create( ClassName )
+		ent:SetPos( SpawnPos )
+		ent:SetAngles(Angle(0,ply:EyeAngles().y,0))
+		ent:Spawn()
+		ent:Activate()
+		return ent
+	end
+else
+	surface.CreateFont( "SkyFox-DigitalClock", {
+		font = "Arial", -- Use the font-name which is shown to you by your operating system Font Viewer, not the file name
+		extended = false,
+		size = 50,
+		weight = 500,
+		blursize = 0,
+		scanlines = 0,
+		antialias = true,
+		underline = false,
+		italic = false,
+		strikeout = false,
+		symbol = false,
+		rotary = false,
+		shadow = false,
+		additive = false,
+		outline = false,
+	} )
+	function ENT:Initialize()
+		self.ClockBase = ClientsideModel("models/maxofs2d/hover_plate.mdl",RENDERGROUP_TRANSLUCENT)
+		self.ClockBase:SetPos(self:LocalToWorld(Vector(0,0,-6.2)))
+		self.ClockBase:SetAngles(self:GetAngles())
+		self.ClockBase:SetParent(self)
+		self.ClockBase:SetNoDraw(true)
+	end
+	function ENT:OnRemove()
+		SafeRemoveEntity(self.ClockBase)
+	end
+	local cos,sin,rad,round = math.cos,math.sin,math.rad,math.Round
+	local mat = Material("phoenix_storms/glass")
+	local matBase = Material("effects/splashwake1")
+	local matBeam = Material( "effects/lamp_beam" )
+	function ENT:Draw()
+		-- Render glasscube
+			render.SetBlend(0.5)
+			render.MaterialOverride(mat)
+				self:DrawModel()
+			render.MaterialOverride()
+			render.SetBlend(1)
+		-- Render clockbase
+			if not IsValid(self.ClockBase) then
+				self.ClockBase = ClientsideModel("models/maxofs2d/hover_plate.mdl",RENDERGROUP_TRANSLUCENT)
+				self.ClockBase:SetParent(self)
+				self.ClockBase:SetNoDraw(true)
+			end
+			self.ClockBase:DrawModel()
+			if not IsValid(self.ClockBase:GetParent()) then
+				self.ClockBase:SetParent(self)
+				self.ClockBase:SetPos(self:LocalToWorld(Vector(0,0,-6.2)))
+				self.ClockBase:SetAngles(self:GetAngles())
+			end
+		-- Render checks
+			if ( halo.RenderedEntity() == self ) then return end
+			if not StormFox then return end
+			if not StormFox.Time then return end
+			if not StormFox.Weather then return end
+		-- Draw holo-light
+			local a = self:GetAngles()
+			local f = math.random(78,80)
+			local r = math.random(10)
+			local col = Color(155 - r,155 - r,255)
+			cam.Start3D2D(self:LocalToWorld(Vector(0,0,-4.6)),self:GetAngles(),0.1)
+				surface.SetDrawColor(col)
+				surface.SetMaterial(matBase)
+				surface.DrawTexturedRectRotated(0,0,100,100,SysTime() * 10)
+				surface.DrawTexturedRectRotated(0,0,100,100,SysTime() * -12)
+			cam.End3D2D()
+		
+			render.SetMaterial(matBeam)
+			render.DrawBeam( self:LocalToWorld(Vector(0,0,-5)), self:LocalToWorld(Vector(0,0,5)), 18 - math.random(1), 0, 0.9, col )
+	
+		-- Draw Display
+		cam.Start3D2D(self:GetPos(),Angle(180,EyeAngles().y + 90,-a.p -90),0.07)
+			local _showWeather = CurTime() % 14 < 7
+			if _showWeather then
+				surface.SetDrawColor(col)
+				surface.SetMaterial(StormFox.Weather.GetIcon())
+				surface.SetTextColor(col)
+				surface.SetFont("SkyFox-DigitalClock")
+				local temp = round(StormFox.Temperature.GetDisplay() ,1) .. StormFox.Temperature.GetDisplaySymbol()
+
+				text_length = surface.GetTextSize(temp)
+				surface.SetTextPos(-text_length / 2,-30)
+				surface.DrawText(temp)
+				surface.DrawTexturedRect(-60,-60,40,40)
+			else
+				surface.SetTextColor(col)
+				surface.SetFont("SkyFox-DigitalClock")
+				local text = StormFox.Time.GetDisplay()
+				local text_length = surface.GetTextSize(text)
+				surface.SetTextPos(-text_length / 2,-30)
+				surface.DrawText(text)
+			end
+		cam.End3D2D()
+	end
+end
+
+
