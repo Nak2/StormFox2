5064274d87a5e644a166b51fe842aed572a04080
Nak <nak@live.dk>
2021-07-31 02:41:56 +0200

    Added LightTool

---
+++ lua/entities/stormfox_streetlight_invisible.lua
@@ -0,0 +1,240 @@
+AddCSLuaFile()
+
+ENT.Type = "anim"
+ENT.Base = "base_anim"
+ 
+ENT.PrintName		= "Invisible streetlight"
+ENT.Author			= "Nak"
+ENT.Purpose			= "Light area up"
+ENT.Instructions	= "Place it somewhere"
+ENT.Category		= "StormFox2"
+
+ENT.Editable		= true
+ENT.Spawnable		= false
+ENT.AdminOnly		= true
+
+ENT.RenderGroup = RENDERGROUP_TRANSLUCENT
+
+local RENDER_DISTANCE = 3500 ^ 2
+
+function ENT:Initialize()
+	if SERVER then
+		self:SetModel( "models/hunter/blocks/cube025x025x025.mdl" )
+		self:PhysicsInit( SOLID_VPHYSICS )      -- Make us work with physics,
+		self:SetMoveType( MOVETYPE_NONE )   -- after all, gmod is a physics
+		self:SetSolid( SOLID_VPHYSICS )         -- Toolbox
+		self:DrawShadow(false)
+	end
+end
+
+function ENT:SetupDataTables()
+	self:NetworkVar( "Int", 0, "LightType" )
+end
+
+if CLIENT then
+	local c_outline = Material("model_color")
+	function ENT:DrawSelfCheck()
+		local wep = LocalPlayer():GetActiveWeapon()
+		if not wep or not IsValid(wep) then return end
+		if wep:GetClass() ~= "sf2_tool" then return end
+		render.SetLightingMode(2)
+			render.MaterialOverride(c_outline)
+			self:DrawModel()
+			render.MaterialOverride()
+		render.SetLightingMode(0)
+	end
+	-- We use a variable to tell if we should render any lights nearby. Since it can be is costly.
+	local m_render = false
+	local sorter = function(a,b)
+		return a[2] < b[2]
+	end
+
+	local Max_PointLight 	= 10
+	local Max_SpotLight 	= 4
+	local Max_ShadowLight 	= 2
+	local Max_Fake			= 40
+
+	local INVALID 	= 0
+	local POINTLIGHT= 1
+	local SPOTLIGHT = 2
+	local FAKESPOT	= 3
+
+	plights = plights or {
+		ents = {},
+		used = 0
+	}
+	-- Handles and returns a project texture.
+	-- Returns the projected texture and ID
+	local function RequestLight()
+		plights.used = plights.used + 1
+		if plights.used > Max_SpotLight then return end -- Max 4
+		local pfent = plights.ents[plights.used]
+		if IsValid(pfent) then
+			return pfent, plights.used
+		end
+		plights.ents[plights.used] = ProjectedTexture()
+		pfent = plights.ents[plights.used]
+		if not IsValid(fent) then return end
+		pfent:SetTexture( "effects/flashlight001" )
+		pfent:SetBrightness(2)
+		pfent:SetEnableShadows( plights.used < Max_ShadowLight )
+		return pfent, plights.used
+	end
+	local cost = {}
+	local row = 0
+	hook.Add("PostDrawTranslucentRenderables", "StormFox2.Streetlights", function(a, b, c)
+		if a or b or c or not (m_render or plights.ents[1]) then return end
+		-- Check the light-range
+		local b_on = StormFox2.Map.GetLight() < 20
+		if row < 6 and b_on then -- On
+			row = math.min(row + FrameTime() * 0.75, 6)
+		elseif row > 0 and not b_on then
+			row = math.max(row - FrameTime() * 0.95, 0)
+		end
+		if row <= 0 and not plights.ents[1] then return end -- No lights are on. No need to calculate things
+		local view =  StormFox2.util.GetCalcView()
+		local rp = view.pos + view.ang:Forward() * 250
+		-- Sort the lights. So we get the closest first
+		local t = {}
+		local nID = math.Round(row, 0)
+		for k, v in ipairs(ents.FindByClass("stormfox_streetlight_invisible")) do
+			if v:EntIndex() % 6 >= row then continue end 
+			table.insert(t, {v,v:GetPos():DistToSqr(rp) - (v._tDis2 or 0)})
+		end
+		table.sort(t, sorter)
+		-- Setup cost calculation
+		cost[1] = Max_PointLight
+		cost[2] = Max_SpotLight
+		cost[3] = Max_Fake
+		local vAng = view.ang
+		local vNorm = vAng:Forward()
+		plights.used = 0
+		-- Draw lights
+		local fD = (1 - (StormFox2.Fog.GetDistance() / 6000))
+		for _, tab in ipairs(t) do
+			local n = 1 - (tab[2] / RENDER_DISTANCE)
+			if n <= 0 then continue end
+			tab[1]:DrawLight(n * (0.85 + 0.45 * fD),view.pos,vAng,vNorm)
+		end
+		for i = 4, 1, -1 do
+			if not IsValid(plights.ents[i]) then continue end
+			if plights.used < i then
+				plights.ents[i]:Remove()
+				plights.ents[i] = nil
+			else
+				break
+			end
+		end
+		m_render = false
+	end)
+	function ENT:DrawTranslucent()
+		if not m_render then -- Wait until some lights is in render
+			local rp = StormFox2.util.RenderPos()
+			local d = rp:DistToSqr(self:GetPos())
+			if d > RENDER_DISTANCE then return end
+		end
+		self:DrawSelfCheck() -- Draw if tool is out
+		m_render = true
+	end
+	local m_lamp = Material("stormfox2/effects/light_beam")
+	local col = Color(255,255,255,155)
+	function ENT:TraceDown()
+		if self._tPos then return self._tPos, self._tDis end
+		local norm = -self:GetAngles():Up()
+		local tr = util.TraceLine({
+			start = self:GetPos() + norm * 100,
+			endpos = self:GetPos() + norm * 1000,
+			filter = self,
+			mask = MASK_SOLID_BRUSHONLY
+		})
+		self._tPos = tr.HitPos or self:GetPos()
+		self._tDis = self._tPos:Distance(self:GetPos())
+		self._tDis2 = self._tDis^2
+		return self._tPos, self._tDis
+	end
+
+	local m_spot = Material('stormfox2/effects/spotlight')
+	local m_flash = Material('stormfox2/effects/flashlight_a')
+	function ENT:DrawPoint(dist, add, size)
+		size = size or 80
+		col.a = 255 * dist
+		render.SetMaterial(m_spot)
+		if add then
+			render.DrawSprite(self:GetPos() + add, size, size, col)
+		else
+			render.DrawSprite(self:GetPos(), size, size, col)
+		end
+	end
+	local c_flash = Color(255,255,255,255)
+	function ENT:DrawSpot(dist, fake)
+		dist = (dist - .5) * 2
+		if dist <= 0 then return end
+		if not fake then
+			local pEnt, id = RequestLight()
+			if not IsValid(pEnt) then return end
+			local bright = math.Round(dist * 2, 1)
+			if self._lastB == bright and (self._lastID or -1) == id then return end
+			self._lastB = bright
+			pEnt:SetBrightness(bright)
+			local pos, dis = self:TraceDown()
+			local up = self:GetAngles():Up()
+			local aDown = -up:Angle()
+			pEnt:SetPos(self:GetPos() + up * -20)
+			pEnt:SetAngles(aDown)
+			pEnt:SetFarZ( dis * 1.2 ) 
+			pEnt:SetTexture( "effects/flashlight001" )
+			pEnt:Update()
+			self._lastID = id
+		else
+			local pos, dis = self:TraceDown()
+			local up = self:GetAngles():Up()
+			c_flash.a = 15 * dist
+			if c_flash.a <= 0 then return end
+			local s = (dis - 20) * 1.6
+			render.SetMaterial(m_flash)
+			render.DrawQuadEasy(pos + up, up, s, s, c_flash, 0)
+		end
+	end
+	-- Dist goes from 1 to 0. Where 0 is RENDER_DISTANCE units away.
+	function ENT:DrawBeam(dist, vPos)
+		local vNorm2 = (vPos - self:GetPos()):Angle():Forward()
+		local pos, dis = self:TraceDown()
+		local up = self:GetAngles():Up()
+		local dot = up:Dot(vNorm2)
+		local abs_dot = math.abs(dot)
+		if abs_dot < 0.9 then
+			col.a = dist * 255 * math.Clamp(0.9 - abs_dot,0,1)
+			render.SetMaterial(m_lamp)
+			--local m = (pos - self:GetPos()):GetNormalized()
+			--render.StartBeam( 3 )
+			--	render.AddBeam( self:GetPos(), dis * 0.8, 0, col )
+			--	render.AddBeam( self:GetPos() + m * dis / 8,dis * 0.8, 0.1, col )
+			--	render.AddBeam( pos			 , dis * 0.8, 0.99, col )
+			--render.EndBeam()
+			render.DrawBeam(self:GetPos(),pos , dis * 0.8, 0, 0.99, col)
+		end
+		if dot < 0 then
+			self:DrawPoint(dist * -dot, vNorm2 * 15 - up * 2, dis / 3)
+		end
+	end
+	function ENT:DrawLight(dist,vPos,vAng,vNorm)
+		dist = math.min(dist, 1)
+		local lType = self:GetLightType()
+		if lType == 0 then return end -- Invalid
+		local fake = false
+		if cost[lType] <= 0 then-- Ignore
+			if lType == SPOTLIGHT then
+				fake = true
+			end
+		end 
+		cost[lType] = cost[lType] - 1
+		if lType == POINTLIGHT then
+			self:DrawPoint(dist)
+		elseif lType == FAKESPOT then
+			self:DrawBeam(dist,vPos,vNorm)
+		elseif lType == SPOTLIGHT then
+			self:DrawBeam(dist,vPos,vNorm)
+			self:DrawSpot(dist, fake)
+		end
+	end
+end
---
+++ lua/weapons/sf2_tool/settings/light_editor.lua
@@ -0,0 +1,290 @@
+local TOOL = {}
+TOOL.PrintName = "#sf_tool.light_editor"
+TOOL.ToolTip = "#sf_tool.light_editor.desc"
+TOOL.NoPrintName = false
+-- TOOL.ShootSound = Sound("weapons/irifle/irifle_fire2.wav")
+
+local SPAWN 	= 0
+local DELETE 	= 1
+local SPAWN_ALL = 2
+local DELETE_ALL= 3
+
+local INVALID 	= 0
+local POINTLIGHT= 1
+local SPOTLIGHT = 2
+local FAKESPOT	= 3
+
+local t_models = {}
+	t_models['models/props_c17/lamppost03a_off.mdl'] 			= {Vector(0,94,440), 	Angle(0,0,0),	SPOTLIGHT}
+	t_models['models/sickness/evolight_01.mdl'] 				= {Vector(0,-80,314),	Angle(0,0,0), 	SPOTLIGHT}
+	t_models['models/props_lighting/lightfixture02.mdl'] 		= {Vector(50,0,-10), 	Angle(30,0,0),	FAKESPOT}
+	t_models['models/sickness/parkinglotlight.mdl'] 			= {Vector(0,30,284),	Angle(0,0,0),	FAKESPOT,	Vector(0,-30,284)}
+	t_models['models/props/de_inferno/light_streetlight.mdl'] 	= {Vector(0,0,150),		Angle(0,0,0),	POINTLIGHT}
+	t_models['models/props/cs_office/light_inset.mdl'] 			= {Vector(0,0,-3),		Angle(0,0,0),	POINTLIGHT}
+
+local function IsLightNear(pos)
+	local t = {}
+	for k,v in ipairs(ents.FindInSphere(pos, 30)) do
+		if v:GetClass() == "stormfox_streetlight_invisible" then
+			return v
+		end
+	end
+end
+
+local function SpawnMissingLight(pos, ang, i_type)
+	if IsLightNear(pos) then return end
+	local ent = ents.Create("stormfox_streetlight_invisible")
+		ent:SetPos(pos)
+		ent:SetAngles(ang)
+		ent:Spawn()
+		ent:SetLightType(i_type)
+	return ent
+end
+
+local function StaticLocal(v, pos, ang)
+	return LocalToWorld(pos * (v.Scale or 1), ang, v.Origin, v.Angles)
+end
+
+local function StaticLightPos(v)
+	local tab = t_models[v.PropType]
+	if not tab then return end -- Unknown
+	local pos, ang = StaticLocal(v, tab[1] * v.Scale, tab[2])
+	local spos
+	if tab[4] then
+		spos, ang = StaticLocal(v, tab[4] * v.Scale, tab[2])
+	end
+	return pos, ang, spos
+end
+
+local sorter = function(a,b)
+	return a[5]<b[5]
+end
+
+local function FindStaticProps(pos, dis)
+	local ls = StormFox2.Map.FindStaticsInSphere(pos, dis)
+	local t = {}
+	for k, v in ipairs(ls) do
+		if t_models[v.PropType] then
+			table.insert(t, {v.PropType, v.Origin, v.Angles, v.Scale, v.Origin:DistToSqr(pos)})
+		else
+		end
+	end
+	if #t < 1 then return end
+	if #t < 2 then return t[1][1],t[1][2],t[1][3],t[1][4] end
+	table.sort(t, sorter)
+	return t[1][1],t[1][2],t[1][3],t[1][4]
+end
+
+-- Returns a mdl, pos, ang and scale if found
+local function FindTraceTarget(tr)
+	local ent = tr.Entity
+	if not ent then return end
+	if ent:IsWorld() then -- Static-Prop?
+		if tr.HitTexture ~= "**studio**" then return end -- Not a static prop
+		return FindStaticProps(tr.HitPos, 200)
+	elseif IsValid(ent) and t_models[ent:GetModel()] then-- Prop
+		return ent:GetModel(), ent:GetPos(), ent:GetAngles(), ent:GetModelScale()
+	end
+end
+
+if SERVER then
+	-- Spawns all missing lights for said model
+	local function SpawnMissingLights(mdl)
+		if not t_models[mdl] then return end
+		local all_static = StormFox2.Map.StaticProps()
+		for k, v in pairs(all_static) do
+			if v.PropType ~= mdl then continue end
+			local pos, ang, pos2 = StaticLightPos(v)
+			SpawnMissingLight(pos, ang, t_models[mdl][3])
+			if pos2 then
+				SpawnMissingLight(pos2, ang, t_models[mdl][3])
+			end
+		end
+	end
+	-- Deletes all light for said model
+	local function DeleteAllLights(mdl)
+		if not t_models[mdl] then return end
+		local all_static = StormFox2.Map.StaticProps()
+		for k, v in pairs(all_static) do
+			if v.PropType ~= mdl then continue end
+			local pos, ang, pos2 = StaticLightPos(v)
+			SafeRemoveEntity(IsLightNear(pos))
+			if pos2 then
+				SafeRemoveEntity(IsLightNear(pos2))
+			end
+		end
+	end
+
+	local popsnd = Sound("garrysmod/balloon_pop_cute.wav")
+	function TOOL:SendFunc( a, b, c, d )
+		if a == SPAWN and type(b) == "number" and c and d then -- Spawn, n_type, pos, ang
+			local ent = ents.Create("stormfox_streetlight_invisible")
+			ent:SetPos(c)
+			ent:SetAngles(d)
+			ent:Spawn()
+			ent:SetLightType(b)
+			self:EmitSound("weapons/ar2/ar2_reload_rotate.wav")
+		elseif a == DELETE and IsValid(b) and b.GetClass then -- Delete, entity
+			if b:GetClass()~="stormfox_streetlight_invisible" then return end -- Can't delete things
+			b:EmitSound(popsnd)
+			SafeRemoveEntity(b)
+		elseif a == SPAWN_ALL then
+			SpawnMissingLights(b)
+			self:EmitSound("weapons/ar2/ar2_reload_rotate.wav")
+		elseif a == DELETE_ALL then
+			DeleteAllLights(b)
+			self:EmitSound("weapons/ar2/ar2_reload_rotate.wav")
+		end
+	end
+else
+	function TOOL:SpawnLight(n_type, pos, ang, double)
+		self.SendFunc( SPAWN, n_type, pos, ang )
+		if double then self.SendFunc( SPAWN, n_type, double, ang ) end
+	end
+	function TOOL:DeleteLight( ent )
+		if not IsValid(ent) then return end
+		self.SendFunc( DELETE, ent )
+	end
+	function TOOL:SpawnAllLights(mdl)
+		if not t_models[mdl] then return end
+		self.SendFunc( SPAWN_ALL, mdl )
+	end
+	function TOOL:DeleteAllLights(mdl)
+		if not t_models[mdl] then return end
+		self.SendFunc( DELETE_ALL, mdl )
+	end
+
+	local selectedData = {
+	}
+	local ghost
+	-- Render is called right after screenrender
+	local v = Vector(440,40,40)
+	local m_lamp = Material('stormfox2/effects/light_beam')
+	local m_spot = Material('stormfox2/effects/spotlight')
+	function TOOL:Render()
+		if not selectedData.Pos then return end
+		if selectedData.Type == SPOTLIGHT or selectedData.Type == FAKESPOT then
+			render.SetMaterial(m_lamp)
+			render.DrawBeam(selectedData.Pos, selectedData.Pos - selectedData.Ang:Up() * 300, 300, 0, 1, color_white)
+			if selectedData.Pos2 then
+				render.DrawBeam(selectedData.Pos2, selectedData.Pos2 - selectedData.Ang:Up() * 300, 300, 0, 1, color_white)
+			end
+		elseif selectedData.Type == POINTLIGHT then
+			render.SetMaterial(m_spot)
+			render.DrawSprite(selectedData.Pos, 80, 80, color_white)
+			if selectedData.Pos2 then
+				render.DrawSprite(selectedData.Pos2, 80, 80, color_white)
+			end
+		end
+	end
+
+	local ghost
+	local col = Color(0,255,0)
+	local ssi_mdl = Model("models/hunter/blocks/cube025x025x025.mdl")
+	local c_outline = "model_color"
+	local a_outline = "models/effects/comball_tape"
+	local default_lighttype = FAKESPOT
+	local function ScanTrace(self,tr)
+		selectedData = {}
+		-- Delete lights if looking at them
+		if IsValid(tr.Entity) and tr.Entity:GetClass() == "stormfox_streetlight_invisible" then
+			ghost = self._swep:SetGhost(tr.Entity:GetModel(), tr.Entity:GetPos(), tr.Entity:GetAngles())
+			self:SetGhostHalo(col)
+			selectedData.deleteEnt = tr.Entity
+		else -- Check for staticprops / props
+			local mdl, pos, ang, scale = FindTraceTarget(tr)
+			if mdl then -- If valid target
+				selectedData.Model = mdl
+				local tab = t_models[mdl]
+				ghost = self._swep:SetGhost(mdl, pos, ang)
+				ghost:SetMaterial(a_outline)
+				if ghost then -- If ghost
+					-- Check if ent is there
+					ghost:SetModelScale(scale)
+					ghost:SetColor(Color(255,255,255,255))
+					selectedData.Pos = ghost:LocalToWorld(tab[1] * scale)
+					selectedData.Ang =  ghost:LocalToWorldAngles(tab[2])
+					selectedData.Pos2 = tab[4] and ghost:LocalToWorld(tab[4] * scale)
+					selectedData.Type = tab[3]
+					local b = IsLightNear(selectedData.Pos)
+					local c = selectedData.Pos2 and IsLightNear(selectedData.Pos2)
+					if b then
+						self:SetGhostHalo(col)
+						selectedData.deleteEnt = b
+						selectedData.deleteEnt2 = c						
+					else
+						self:SetGhostHalo(color_white)
+					end
+				end
+			else -- Invalid target
+				selectedData.Type = default_lighttype
+				local ang = tr.HitNormal:Angle()
+				ang:RotateAroundAxis(ang:Right(), 90)
+				ghost = self._swep:SetGhost(ssi_mdl, tr.HitPos, ang)
+				if ghost then
+					ghost:SetMaterial(c_outline)
+					selectedData.Pos = ghost:GetPos()
+					selectedData.Ang = (input.IsKeyDown( KEY_LCONTROL ) or input.IsKeyDown( KEY_RCONTROL )) and Angle(0,0,0) or ghost:GetAngles()
+					if default_lighttype == POINTLIGHT then
+						selectedData.Pos = selectedData.Pos + tr.HitNormal
+					end
+				end
+			end
+		end
+	end
+
+	do
+		local m1 = Material("effects/lamp_beam")
+		local m2 = Material("sprites/glow04_noz")
+		local m3 = Material("effects/flashlight001")
+		function TOOL:ScreenRender( w, h )
+			ScanTrace(self,LocalPlayer():GetEyeTrace())
+			surface.SetDrawColor(color_white)
+			surface.DrawOutlinedRect(w * 0.1,h * 0.2,w * 0.8,h * 0.7)
+			local l_type = selectedData.Type or default_lighttype
+			surface.SetDrawColor(255,255,255)
+			if l_type == POINTLIGHT then
+				surface.SetMaterial(m2)
+			elseif l_type == FAKESPOT then
+				surface.SetMaterial(m1)
+			elseif l_type == SPOTLIGHT then
+				surface.SetMaterial(m3)
+				surface.DrawTexturedRect(w * 0.1, h * 0.4, w * 0.8, h * 0.7)
+				surface.SetMaterial(m1)
+			end
+			
+			surface.DrawTexturedRect(w * 0.1, h * 0.2, w * 0.8, h * 0.7)
+		end
+	end
+	function TOOL:LeftClick(tr)
+		if not IsValid(ghost) then return end
+		local select_all =  input.IsKeyDown( KEY_LCONTROL ) or input.IsKeyDown( KEY_RCONTROL )
+		-- If we hold control in. Spawn all lights for given model
+		if select_all and selectedData.Model then
+			if selectedData.Model then
+				self:SpawnAllLights(selectedData.Model)
+				return
+			end
+		elseif not selectedData.deleteEnt and selectedData.Pos then -- Only spawn light, if we aren't looking at an entity atm
+			self:SpawnLight(selectedData.Type, selectedData.Pos, selectedData.Ang, selectedData.Pos2)
+		--	selectedData.Pos = nil
+		end
+	end
+	function TOOL:RightClick(tr)
+		local select_all =  input.IsKeyDown( KEY_LCONTROL ) or input.IsKeyDown( KEY_RCONTROL )
+		if select_all then
+			if selectedData.Model then
+				self:DeleteAllLights(selectedData.Model)
+			end
+			return
+		end
+		if selectedData.deleteEnt then
+			self:DeleteLight(selectedData.deleteEnt)
+			self:DeleteLight(selectedData.deleteEnt2)
+		else
+			default_lighttype = default_lighttype + 1
+			if default_lighttype > 3 then default_lighttype = 1 end
+		end
+	end
+end
+return TOOL
